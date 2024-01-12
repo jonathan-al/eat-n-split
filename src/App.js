39121fb708c29f3a7dddf5d5a84fabb246351f34
Jonathan Alcantara <jonathan.alcantara@improving.com>
2024-01-12 17:26:34 -0600

    create friend list and form to add friends

--- src/App.js
+++ src/App.js
@@ -1,5 +1,89 @@
+import "./index.css"
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+]
+
+const FormAddFriend = () => {
+  return (
+    <form>
+      <label>👫Friend name</label>
+      <input type="text" />
+
+      <label>🖼️Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  )
+}
+
+const Button = ({ children }) => {
+  return <button className="button">{children}</button>
+}
+
+const Friend = ({ friend }) => {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}€
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <Button>Select</Button>
+    </li>
+  )
+}
+
+const FriendList = () => {
+  const friends = initialFriends
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  )
+}
+
 const App = () => {
-  return <div>Hello React!</div>
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+        <FormAddFriend />
+      </div>
+    </div>
+  )
 }
 
 export default App
